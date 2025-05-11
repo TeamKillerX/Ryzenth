32eb52bb5b1c82e20bfc834078070cc5b96e6ad0
Randy Dev <killerx@randydev.my.id>
2025-05-11 19:50:41 +0700

    _

--- Ryzenth/_synchisded.py
+++ Ryzenth/_synchisded.py
@@ -31,6 +31,25 @@ class RyzenthXSync:
         self.base_url = base_url.rstrip("/")
         self.headers = {"x-api-key": self.api_key}
 
+    class ImagesAsync:
+        def __init__(self, parent):
+            self.parent = parent
+
+        def generate(self, params: QueryParameter):
+            url = f"{self.parent.base_url}/v1/flux/black-forest-labs/flux-1-schnell"
+            try:
+                response = httpx.get(
+                    url,
+                    params=params.dict(),
+                    headers=self.parent.headers,
+                    timeout=30
+                )
+                response.raise_for_status()
+                return response.content
+            except httpx.HTTPError as e:
+                LOGS.error(f"[SYNC] Error fetching from images {e}")
+                return None
+
     def send_downloader(self, switch_name: str = None, params: QueryParameter = None, list_key=False):
         dl_dict = {
             "teraboxv4": "terabox-v4",
