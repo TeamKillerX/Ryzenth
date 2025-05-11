c4197a3877eb6cba00595a7b75ee9d000ed8d827
Randy Dev <killerx@randydev.my.id>
2025-05-11 16:31:25 +0700

    _

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -31,7 +31,47 @@ class RyzenthXAsync:
         self.base_url = base_url.rstrip("/")
         self.headers = {"x-api-key": self.api_key}
 
-    async def send_message(self, model: str, params: QueryParameter):
+    async def send_downloader(self, switch_name: str = None, params: QueryParameter = None, list_key=False):
+        dl_dict = {
+            "teraboxv4": "terabox-v4",
+            "twitterv3": "twitter-v3",
+            "xnxxinfov2": "xnxx-info-v2",
+            "instagramv4": "instagram-v4",
+            "instagramv3": "instagram-v3",
+            "instagramv2": "instagram-v2",
+            "instagram": "instagram",
+            "twitter": "twitter",
+            "tiktok": "tiktok",
+            "tiktokv2": "tiktok-v2",
+            "facebook": "fb",
+            "snapsave": "snapsave",
+            "savefrom": "savefrom"
+        }
+        if list_key:
+            return list(dl_dict.keys())
+            
+        if not switch_name:
+            raise ValueError("`switch_name` is required. Use `list_key=True` to see all valid options.")
+            
+        model_name = dl_dict.get(switch_name)
+        if not model_name:
+            raise ValueError(f"Invalid switch_name: {switch_name}")
+
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(
+                    f"{self.base_url}v1/dl/{model_name}",
+                    params=params.dict(),
+                    headers=self.headers,
+                    timeout=10
+                )
+                response.raise_for_status()
+                return response.json()
+            except httpx.HTTPError as e:
+                LOGS.error(f"[ASYNC] Error: {str(e)}")
+                return None
+
+    async def send_message(self, model: str = None, params: QueryParameter = None, list_key=False):
         model_dict = {
             "hybrid": "AkenoX-1.9-Hybrid",
             "melayu": "lu-melayu",
@@ -43,6 +83,12 @@ class RyzenthXAsync:
             "lirikend": "lirik-end",
             "alsholawat": "al-sholawat"
         }
+        if list_key:
+            return list(model_dict.keys())
+
+        if not model:
+            raise ValueError("`model` is required. Use `list_key=True` to see all valid options.")
+
         model_param = model_dict.get(model)
         if not model_param:
             raise ValueError(f"Invalid model name: {model}")
