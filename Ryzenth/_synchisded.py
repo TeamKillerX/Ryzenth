5ffced6c6570c105af75a9c484091d45776ead4f
Randy Dev <killerx@randydev.my.id>
2025-05-14 16:14:00 +0700

    _

--- Ryzenth/_synchisded.py
+++ Ryzenth/_synchisded.py
@@ -23,6 +23,7 @@ import httpx
 from box import Box
 
 from Ryzenth.types import DownloaderBy, QueryParameter
+from Ryzenth.helper import WhisperSync
 
 LOGS = logging.getLogger("[Ryzenth] sync")
 
@@ -33,6 +34,7 @@ class RyzenthXSync:
         self.headers = {"x-api-key": self.api_key}
         self.images = self.ImagesSync(self)
         self.what = self.WhatSync(self)
+        self.openai_audio = self.WhisperSync(self)
         self.obj = Box
 
     class WhatSync:
