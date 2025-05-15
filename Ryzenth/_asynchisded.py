e982094ea9d770253767a38a8b789a0e8d743872
Randy Dev <killerx@randydev.my.id>
2025-05-16 05:15:31 +0700

    _

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -23,7 +23,7 @@ import httpx
 from box import Box
 
 from Ryzenth._errors import WhatFuckError
-from Ryzenth.helper import FbanAsync, ImagesAsync, WhatAsync, WhisperAsync
+from Ryzenth.helper import FbanAsync, ImagesAsync, WhatAsync, WhisperAsync, ModeratorAsync
 from Ryzenth.types import DownloaderBy, QueryParameter
 
 LOGS = logging.getLogger("[Ryzenth] async")
@@ -38,6 +38,7 @@ class RyzenthXAsync:
         self.what = WhatAsync(self)
         self.openai_audio = WhisperAsync(self)
         self.federation = FbanAsync(self)
+        self.moderator = ModeratorAsync(self)
         self.obj = Box
 
     async def send_downloader(
