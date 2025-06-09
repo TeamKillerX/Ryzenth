2531de48be455d8b8b780dd646ea540c5d253e74
Randy Dev <killerx@randydev.my.id>
2025-06-09 14:07:55 +0700

    _

---
+++ Ryzenth/tests/test_send_message.py
@@ -0,0 +1,11 @@
+from Ryzenth import ApiKeyFrom
+from Ryzenth.types import QueryParameter
+
+def send_message():
+    ryz = ApiKeyFrom(..., is_ok=True)
+    assert ryz._sync.send_message(
+        "hybrid",
+        QueryParameter(
+            query="hello world!"
+        )
+    )
