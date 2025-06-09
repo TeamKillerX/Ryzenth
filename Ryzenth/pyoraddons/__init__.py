c636f725cb63c004a767016bece1a32d19460ca0
Randy Dev <killerx@randydev.my.id>
2025-06-09 11:20:48 +0700

    _

--- Ryzenth/pyoraddons/__init__.py
+++ Ryzenth/pyoraddons/__init__.py
@@ -43,4 +43,41 @@ def unauthorized_access(
         return wrapper
     return decorator
 
-__all__ = ["unauthorized_access"]
+def callback_unauthorized_access(
+    user_list: list = None,
+    author_only: bool = False,
+    member_only: bool = False
+):
+    "Credits by @xtdevs"
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(client, callback):
+            if author_only and callback.from_user.id != client.me.id:
+                return await callback.answer("This Unauthorized Access Only Owner.", True)
+            if member_only and callback.from_user.id not in user_list:
+                return await callback.answer("This Unauthorized Access Only Members.", True)
+            return await func(client, callback)
+
+        if sum([author_only, member_only]) > 1:
+            raise UnauthorizedAccessError("Only one of author_only, or member_only can be True")
+
+        return wrapper
+    return decorator
+
+def admin_only(enums_type=None):
+    "Credits by @xtdevs"
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(client, message):
+            member = await client.get_chat_member(message.chat.id, message.from_user.id)
+            if member.status not in {enums_type.ADMINISTRATOR, enums_type.OWNER}:
+                return await message.reply_text("Only admin can!")
+            return await func(client, callback)
+        return wrapper
+    return decorator
+
+__all__ = [
+    "unauthorized_access",
+    "callback_unauthorized_access",
+    "admin_only"
+]
