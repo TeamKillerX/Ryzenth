a50787e7e09aec53c0bfe3be319f7e1b1c7da1c1
Randy Dev <killerx@randydev.my.id>
2025-06-11 17:54:55 +0700

    _

--- Ryzenth/_synchisded.py
+++ Ryzenth/_synchisded.py
@@ -38,7 +38,7 @@ from .types import DownloaderBy, QueryParameter
 
 
 class RyzenthXSync:
-    def __init__(self, api_key: str, base_url: str):
+    def __init__(self, api_key: str, base_url: str = "https://randydev-ryu-js.hf.space/api"):
         self.api_key = api_key
         self.base_url = base_url.rstrip("/")
         self.headers = {"x-api-key": self.api_key}
