458c46e6912976b2df70d6d4f72d5cd143026cfd
Randy Dev <killerx@randydev.my.id>
2025-06-11 17:44:48 +0700

    _

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -38,9 +38,9 @@ from .types import DownloaderBy, QueryParameter
 
 
 class RyzenthXAsync:
-    def __init__(self, api_key: str, base_url: str = "https://randydev-ryu-js.hf.space/api"):
+    def __init__(self, api_key: str, base_url: str):
         self.api_key = api_key
-        self.base_url = base_url
+        self.base_url = base_url.rstrip("/")
         self.headers = {"x-api-key": self.api_key}
         self.timeout = 10
         self.params = {}
