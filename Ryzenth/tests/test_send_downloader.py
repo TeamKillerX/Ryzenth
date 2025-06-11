ddd144f196c24618fc8e80b7a2d3ebeb167b071b
Randy Dev <killerx@randydev.my.id>
2025-06-11 17:56:32 +0700

    _

--- Ryzenth/tests/test_send_downloader.py
+++ Ryzenth/tests/test_send_downloader.py
@@ -1,9 +1,9 @@
-from Ryzenth import ApiKeyFrom
+from Ryzenth._synchisded import RyzenthXSync
 from Ryzenth.types import QueryParameter
 
 def test_send_downloader():
-    ryz = ApiKeyFrom("test", base_url="https://x-api-js.onrender.com/api")
-    result = ryz._sync.send_downloader(
+    ryz = RyzenthXSync("test", base_url="https://x-api-js.onrender.com/api")
+    result = ryz.send_downloader(
         switch_name="tiktok-search",
         params=QueryParameter(
             query="cat coding"
