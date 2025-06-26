5535d9c9bfde62affb5fd3c721b3ed6aabee6fa0
Randy Dev <killerx@randydev.my.id>
2025-06-26 22:04:21 +0700

    _

--- Ryzenth/_client.py
+++ Ryzenth/_client.py
@@ -19,58 +19,19 @@
 
 import asyncio
 import json
-import platform
 import random
 import time
 import logging
 import typing as t
-from os import environ, getenv
+from os import getenv
 
 import aiohttp
 import httpx
-from box import Box
 
 from .__version__ import get_user_agent
-from ._asynchisded import RyzenthXAsync
 from ._errors import ForbiddenError, InternalError, ToolNotFoundError, WhatFuckError
 from ._shared import TOOL_DOMAIN_MAP
-from ._synchisded import RyzenthXSync
-from .helper import AutoRetry, Decorators
-
-
-class ApiKeyFrom:
-    def __init__(self, api_key: str = None, is_ok=False):
-        if api_key is Ellipsis:
-            is_ok = True
-            api_key = None
-
-        if not api_key:
-            api_key = environ.get("RYZENTH_API_KEY")
-
-        if not api_key:
-            api_key = "akeno_UKQEQMt991kh2Ehh7JqJYKapx8CCyeC" if is_ok else None
-
-        self.api_key = api_key
-        self.aio = RyzenthXAsync(api_key)
-        self._sync = RyzenthXSync(api_key)
-
-    def something(self):
-        pass
-
-class UrHellFrom:
-    def __init__(self, name: str, only_author=False):
-        self.decorators = Decorators(ApiKeyFrom)
-        self.ai = self.decorators.send_ai(name=name, only_author=only_author)
-
-    def something(self):
-        pass
-
-class FromConvertDot:
-    def __init__(self, obj):
-        self.obj = obj
-
-    def to_dot(self):
-        return Box(self.obj if self.obj is not None else {})
+from .helper import AutoRetry
 
 class RyzenthApiClient:
     def __init__(
