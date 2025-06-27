d5ff0c7a299e78beefa9b1ab3c49e3c6888346b8
Randy Dev <killerx@randydev.my.id>
2025-06-27 17:54:54 +0700

    _

--- Ryzenth/_benchmark.py
+++ Ryzenth/_benchmark.py
@@ -4,22 +4,25 @@ from functools import wraps
 
 logger = logging.getLogger(__name__)
 
-def log_performance(func):
-    @wraps(func)
-    async def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        ok = await func(*args, **kwargs)
-        end_time = time.perf_counter()
-        logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
-        return ok
-    return wrapper
+class Benchmark:
+    @classmethod
+    def _performance(cls, func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = await func(*args, **kwargs)
+            end_time = time.perf_counter()
+            logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
+            return result
+        return wrapper
 
-def sync_log_test(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        ok = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
-        return ok
-    return wrapper
+    @classmethod
+    def _sync(cls, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
+            return result
+        return wrapper
