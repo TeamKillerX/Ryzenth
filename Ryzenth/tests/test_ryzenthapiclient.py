5978a2f8a9b3ea8d6911b10744c430e884affb45
Randy Dev <killerx@randydev.my.id>
2025-06-27 20:07:39 +0700

    _

--- Ryzenth/tests/test_ryzenthapiclient.py
+++ Ryzenth/tests/test_ryzenthapiclient.py
@@ -1,15 +1,16 @@
 from Ryzenth import RyzenthApiClient
 from Ryzenth.enums import ResponseType
 
-clients_httpx = RyzenthApiClient(
+clients = RyzenthApiClient(
     tools_name=["itzpire"],
+    api_key={"itzpire": [{}]},
     rate_limit=100,
     use_default_headers=False,
     use_httpx=True
 )
 
 def test_itzpire():
-    result = clients_httpx.sync_get(
+    result = clients.sync_get(
         tool="itzpire",
         path="/games/siapakah-aku",
         use_type=ResponseType.JSON
