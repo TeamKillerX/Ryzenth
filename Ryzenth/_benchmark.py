ce1cc33a1ece38439e4d830650bc312a4ba1b9d7
Randy Dev <killerx@randydev.my.id>
2025-06-27 17:45:09 +0700

    _

--- Ryzenth/_benchmark.py
+++ Ryzenth/_benchmark.py
@@ -8,16 +8,18 @@ def log_performance(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
-        result = await func(*args, **kwargs)
+        ok = await func(*args, **kwargs)
         end_time = time.perf_counter()
         logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
+        return ok
     return wrapper
 
 def sync_log_test(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
-        result = func(*args, **kwargs)
+        ok = func(*args, **kwargs)
         end_time = time.perf_counter()
         logger.info(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds")
+        return ok
     return wrapper
