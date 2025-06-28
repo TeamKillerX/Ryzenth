f5f4a33866b32e811b76fd6c9cee5a7b63deca3c
Randy Dev <killerx@randydev.my.id>
2025-06-28 23:04:24 +0700

    _

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -132,7 +132,8 @@ class RyzenthXAsync:
             except httpx.HTTPError as e:
                 self.logger.error(f"[ASYNC] Error: {str(e)}")
                 raise WhatFuckError("[ASYNC] Error fetching") from e
-            except httpx.ReadTimeout:
+            except httpx.ReadTimeout as readerr:
+                self.logger.info(f"[ASYNC] Error try again: {str(readerr)}")
                 try:
                     async with aiohttp.ClientSession() as session_aio:
                         async with session_aio.get(
@@ -144,6 +145,7 @@ class RyzenthXAsync:
                             data_json = await response_aio.json()
                             return self.obj(data_json or {}) if dot_access else data_json
                 except Exception as e:
+                    self.logger.error(f"[ASYNC] Error: {str(e)}")
                     raise WhatFuckError("[ASYNC] Error fetching") from e
 
     async def _client_message_get(self, client, params, model_param):
@@ -186,7 +188,8 @@ class RyzenthXAsync:
             except httpx.HTTPError as e:
                 self.logger.error(f"[ASYNC] Error: {str(e)}")
                 raise WhatFuckError("[ASYNC] Error fetching") from e
-            except httpx.ReadTimeout:
+            except httpx.ReadTimeout as readerr:
+                self.logger.info(f"[ASYNC] Error try again: {str(readerr)}")
                 try:
                     async with aiohttp.ClientSession() as session_aio:
                         async with session_aio.get(
@@ -198,4 +201,5 @@ class RyzenthXAsync:
                             data_json = await response_aio.json()
                             return self.obj(data_json or {}) if dot_access else data_json
                 except Exception as e:
+                    self.logger.error(f"[ASYNC] Error: {str(e)}")
                     raise WhatFuckError("[ASYNC] Error fetching") from e
