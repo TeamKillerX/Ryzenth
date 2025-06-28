a5039f3a6d87c94b183cc4f972355c12e6663b75
Randy Dev <killerx@randydev.my.id>
2025-06-28 15:33:18 +0700

    _

--- Ryzenth/__version__.py
+++ Ryzenth/__version__.py
@@ -4,7 +4,7 @@ import platform
 def get_user_agent() -> str:
     return f"Ryzenth/Python-{platform.python_version()}"
 
-__version__ = "2.0.7"
+__version__ = "2.0.8"
 __author__ = "TeamKillerX"
 __title__ = "Ryzenth"
 __description__ = "Ryzenth Python API Wrapper"
