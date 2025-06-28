c915f5fe5992db8cefdec3cf70551b14cc9c95b8
Randy Dev <killerx@randydev.my.id>
2025-06-28 22:26:25 +0700

    _

--- Ryzenth/__version__.py
+++ Ryzenth/__version__.py
@@ -1,10 +1,9 @@
 import platform
 
-
 def get_user_agent() -> str:
     return f"Ryzenth/Python-{platform.python_version()}"
 
-__version__ = "2.0.8"
+__version__ = "2.0.9"
 __author__ = "TeamKillerX"
 __title__ = "Ryzenth"
-__description__ = "Ryzenth Python API Wrapper"
+__description__ = "Ryzenth is a flexible Multi-API SDK with built-in support for API key management and database integration."
