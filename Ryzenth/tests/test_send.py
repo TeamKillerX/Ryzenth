4c7bef75507789aed6532939c0fad3b7b6c98b05
Randy Dev <killerx@randydev.my.id>
2025-06-29 16:23:46 +0700

    Fix one issues two times timeout

--- Ryzenth/tests/test_send.py
+++ Ryzenth/tests/test_send.py
@@ -1,14 +1,5 @@
 from Ryzenth import ApiKeyFrom
-from Ryzenth.types import QueryParameter  # disarankan gunakan absolute import
-
-
-def test_send_message():
-    ryz = ApiKeyFrom(..., is_ok=True)
-    result = ryz._sync.send_message(
-        model="hybrid",
-        params=QueryParameter(query="hello world!")
-    )
-    assert result is not None
+from Ryzenth.types import QueryParameter
 
 def test_send_message_melayu():
     ryz = ApiKeyFrom(..., is_ok=True)
