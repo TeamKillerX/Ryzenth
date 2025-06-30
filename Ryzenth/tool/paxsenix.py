d33919772f504df32a392dc2e5d1cad83aee09f3
Randy Dev <killerx@randydev.my.id>
2025-06-30 16:45:19 +0700

    Update paxsenix.py

--- Ryzenth/tool/paxsenix.py
+++ Ryzenth/tool/paxsenix.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2019-2025 (c) Randy W @xtdevs, @xtsea
+#
+# from : https://github.com/TeamKillerX
+# Channel : @RendyProjects
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# BASED: https://api.paxsenix.biz.id/docs
+
 from .._client import RyzenthApiClient
 from ..enums import ResponseType
 
@@ -17,6 +38,7 @@ class Paxsenix:
         return await self.start()
 
     async def ChatCompletions(self, **kwargs):
+        # https://api.paxsenix.biz.id/docs#endpoint-e42b905
         clients = self._service_new()
         return await clients.post(
             tool="paxsenix",
@@ -31,4 +53,10 @@ class Paxsenix:
             path="/v1/models",
             **kwargs
         )
-        
+    async def GeminiRealtime(self, **kwargs):
+        clients = self._service_new()
+        return await clients.get(
+            tool="paxsenix",
+            path="/ai/gemini-realtime",
+            **kwargs
+        )
