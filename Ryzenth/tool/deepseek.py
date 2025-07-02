19d87b2f5880d4b09ed6faa9ee114ce881b61ed7
Randy Dev <killerx@randydev.my.id>
2025-07-02 12:44:44 +0700

    _

--- Ryzenth/tool/deepseek.py
+++ Ryzenth/tool/deepseek.py
@@ -27,7 +27,7 @@ from ..enums import ResponseType
 from ..helper import AutoRetry
 
 
-class DeepSeek:
+class DeepSeekClient:
     def __init__(self, *, api_key: str):
         self._api_key = api_key
 
