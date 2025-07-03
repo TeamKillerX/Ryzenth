71e14723e1778f9a803619375a2ceceec60c193c
Randy Dev <killerx@randydev.my.id>
2025-07-03 13:02:58 +0700

    _

--- Ryzenth/tests/test_ryzenthapiclient.py
+++ Ryzenth/tests/test_ryzenthapiclient.py
@@ -4,8 +4,8 @@ from Ryzenth import RyzenthApiClient
 from Ryzenth.enums import ResponseType
 
 clients = RyzenthApiClient(
-    tools_name=["itzpire"],
-    api_key={"itzpire": [{}]},
+    tools_name=["yogik"],
+    api_key={"yogik": [{}]},
     rate_limit=100,
     use_httpx=True # Fixed Aiohttp RuntimeError: no running event loop
 )
@@ -13,8 +13,8 @@ clients = RyzenthApiClient(
 @pytest.mark.asyncio
 async def test_itzpire():
     result = await clients.get(
-        tool="itzpire",
-        path="/games/siapakah-aku",
+        tool="yogik",
+        path="/api/status",
         use_type=ResponseType.JSON
     )
     assert result is not None
