29255304faf440ed19666e8d3529c6c8b04e7616
Randy Dev <killerx@randydev.my.id>
2025-07-04 16:55:19 +0700

    _

--- Ryzenth/tool/grok.py
+++ Ryzenth/tool/grok.py
@@ -20,7 +20,7 @@
 # BASED API: https://api.x.ai/v1
 
 import logging
-
+import typing as t
 from .._benchmark import Benchmark
 from .._client import RyzenthApiClient
 from ..enums import ResponseType
@@ -39,4 +39,66 @@ class GrokClient:
             use_default_headers=True,
             **kwargs
         )
+
     #TODO: HERE ADDED
+    @Benchmark.performance(level=logging.DEBUG)
+    @AutoRetry(max_retries=3, delay=1.5)
+    async def chat_completions(
+        self,
+        *,
+        messages: list[dict],
+        model: str = "grok-3-mini-latest",
+        reasoning_effort: str = "low",
+        temperature: t.Union[int, float] = 0.7,
+        stream: bool = False,
+        **kwargs
+    ):
+        """
+        When to Use Reasoning
+        Use grok-3-mini or grok-3-mini-fast: For tasks that can benefit from logical reasoning (such as meeting scheduling or math problems). Also great for tasks that don't require deep domain knowledge about a specific subject (eg basic customer support bot).
+        Use grok-3 or grok-3-fast: For queries requiring deep domain expertise or world knowledge (eg healthcare, legal, finance).
+
+        Note:
+        Reasoning is only supported by 
+        grok-3-mini and grok-3-mini-fast.
+        The Grok 3 models grok-3 (reasoning_effort) and grok-3-fast do not support reasoning
+
+        source: https://docs.x.ai/docs/guides/reasoning
+        """
+        clients = await self.start()
+        return await clients.post(
+            tool="grok",
+            path="/chat/completions",
+            json=clients.get_kwargs(
+                messages=messages,
+                model=model,
+                stream=stream,
+                temperature=temperature,
+                reasoning_effort=reasoning_effort
+            ),
+            **kwargs
+        )
+
+    @Benchmark.performance(level=logging.DEBUG)
+    @AutoRetry(max_retries=3, delay=1.5)
+    async def images_generations(
+        self,
+        *,
+        prompt: str,
+        model: str = "grok-2-image",
+        response_format: str = "url",
+        n: int = 1,
+        **kwargs
+    ):
+        clients = await self.start()
+        return await clients.post(
+            tool="grok",
+            path="images/generations",
+            json=clients.get_kwargs(
+                model=model,
+                prompt=prompt,
+                response_format=response_format,
+                n=n
+            ),
+            **kwargs
+        )
