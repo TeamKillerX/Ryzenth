ae9428a5186c0cd45ed5e3a72866a9935970ff12
Randy Dev <killerx@randydev.my.id>
2025-07-25 14:37:00 +0700

    Update __init__.py

--- Ryzenth/__init__.py
+++ Ryzenth/__init__.py
@@ -21,10 +21,12 @@ from . import *
 from .__version__ import __version__
 from ._base_client import ApiKeyFrom, FromConvertDot, UrHellFrom
 from ._client import RyzenthApiClient
+from ._new_client import RyzenthAuthClient
 
 __all__ = [
   "ApiKeyFrom",
   "RyzenthApiClient",
+  "RyzenthAuthClient",
   "UrHellFrom",
   "FromConvertDot"
 ]
