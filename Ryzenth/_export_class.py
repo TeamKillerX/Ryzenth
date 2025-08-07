f8a0dda35e00fe2629d9be11cfb367dc8a38c2d6
Randy Dev <killerx@randydev.my.id>
2025-08-07 21:05:53 +0700

    _

--- Ryzenth/_export_class.py
+++ Ryzenth/_export_class.py
@@ -2,6 +2,17 @@ import io
 import base64
 from ._errors import WhatFuckError
 
+class ResponseResult:
+    def __init__(self, client=None, response=None):
+        self._client = client
+        self._response = response
+
+    async def to_result(self):
+        return self._client.dict_convert_to_dot(self._response).data.choices[0].message.content
+
+    async def to_json(self):
+        return self._response
+
 class GeneratedImage:
     def __init__(self, client, content, file_path, logger):
         self._client = client
@@ -10,6 +21,9 @@ class GeneratedImage:
         self._logger = logger
 
     async def to_save(self):
+        if not self._content:
+            raise WhatFuckError("No content available")
+
         saved_path = await self._client.to_image_class(self._content, self._file_path)
         if not saved_path:
             raise WhatFuckError("Failed to save generated image")
@@ -17,7 +31,15 @@ class GeneratedImage:
         return saved_path
       
     async def to_base64(self):
+        if not self._content:
+            raise WhatFuckError("No content available")
+
       return base64.b64encode(self._content).decode()
       
     async def to_fileobj(self):
-      return io.BytesIO(self._content)
+        if not self._content:
+            raise WhatFuckError("No content available")
+        return io.BytesIO(self._content)
+        
+    def __repr__(self):
+        return f"<GeneratedImage path={self._file_path}>"
