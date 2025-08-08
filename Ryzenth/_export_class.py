42fa7284c5bc7da14776b4fd2f556e75b09c863d
Randy Dev <killerx@randydev.my.id>
2025-08-08 21:14:59 +0700

    _

--- Ryzenth/_export_class.py
+++ Ryzenth/_export_class.py
@@ -1,8 +1,7 @@
 import base64
 import io
-
+import asyncio
 from ._errors import WhatFuckError
-
 
 class ResponseResult:
     def __init__(self, client=None, response=None, is_ultimate: bool = False):
@@ -24,6 +23,21 @@ class GeneratedImage:
         self._content = content
         self._file_path = file_path
         self._logger = logger
+
+    async def create_task_id_and_wait_from_qwen(self, return_url: bool = False):
+        while True:
+            task_id = self._content["output"]["task_id"]
+            result = await self._client.get(
+                tool="alibaba",
+                path=f"/api/v1/tasks/{task_id}",
+                timeout=100
+            )
+            status = result["output"]["task_status"]
+            if status == "SUCCEEDED":
+                return result["output"]["results"][0]["url"] if return_url else result["output"]
+            elif status == "FAILED":
+                raise WhatFuckError("Qwen Failed to generate image")
+            await asyncio.sleep(1)
 
     async def to_save(self):
         if not self._content:
