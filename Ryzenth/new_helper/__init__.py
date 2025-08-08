4ccbe5f4377098f3a0bf719dd2c601a8fc2f5d30
Randy Dev <killerx@randydev.my.id>
2025-08-09 03:24:35 +0700

    _

--- Ryzenth/new_helper/__init__.py
+++ Ryzenth/new_helper/__init__.py
@@ -1,8 +1,13 @@
 from ._chats import ChatOrgAsync
 from ._images import ImagesOrgAsync
 from ._qwen_images import ImagesQwenAsync
+from ._qwen_videos import VideosQwenAsync
 
-__all__ = ["ImagesOrgAsync", "ChatOrgAsync"]
+__all__ = [
+  "ImagesOrgAsync",
+  "ChatOrgAsync",
+  "VideosQwenAsync"
+]
 
 __author__ = "Randy W @xtdevs, @xtsea"
 __description__ = "Enhanced helper modules for Ryzenth API"
