480ee9cc4ee00e239577cf9b1dae75bc82dbaf56
Randy Dev <killerx@randydev.my.id>
2025-08-09 18:31:16 +0700

    _

--- Ryzenth/new_helper/_qwen_videos.py
+++ Ryzenth/new_helper/_qwen_videos.py
@@ -100,8 +100,8 @@ class VideosQwenAsync:
 
             return GeneratedImageOrVideo(client=client, content=response)
         except Exception as e:
-            self.logger.error(f"Qwen image generation failed: {e}")
-            raise WhatFuckError(f"Qwen image generation failed: {e}") from e
+            self.logger.error(f"Qwen video generation failed: {e}")
+            raise WhatFuckError(f"Qwen video generation failed: {e}") from e
         finally:
             pass
 
