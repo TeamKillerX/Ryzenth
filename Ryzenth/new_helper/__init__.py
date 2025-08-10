4d1b5c19e791ac94542901e519d664310d86802b
Randy Dev <killerx@randydev.my.id>
2025-08-10 13:51:13 +0700

    _

--- Ryzenth/new_helper/__init__.py
+++ Ryzenth/new_helper/__init__.py
@@ -3,12 +3,15 @@ from ._images import ImagesOrgAsync
 from ._qwen_chats import ChatsQwenAsync
 from ._qwen_images import ImagesQwenAsync
 from ._qwen_videos import VideosQwenAsync
+from ._openai_chats import ChatsOpenAIAsync
 
 __all__ = [
-  "ImagesOrgAsync",
   "ChatOrgAsync",
-  "VideosQwenAsync",
-  "ChatsQwenAsync"
+  "ChatsQwenAsync",
+  "ChatsOpenAIAsync",
+  "ImagesQwenAsync",
+  "ImagesOrgAsync",
+  "VideosQwenAsync"
 ]
 
 __author__ = "Randy W @xtdevs, @xtsea"
