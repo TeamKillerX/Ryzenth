ae808a1e8dca49fe2b9b925ffe325fc6b7cecd0a
Randy Dev <killerx@randydev.my.id>
2025-08-25 18:46:37 +0700

    _

--- Ryzenth/new_helper/_openai_chats.py
+++ Ryzenth/new_helper/_openai_chats.py
@@ -89,6 +89,12 @@ class ChatsOpenAIAsync:
             if not response:
                 raise EmptyResponseError("Empty response from chat completion API")
 
+            if instructions is not None:
+                self.logger.warning("Default 'instructions' this warning just ignore")
+
+            if reasoning is not None:
+                self.logger.warning("Default 'reasoning' this warning just ignore")
+
             return ResponseResult(client=client, response=response)
         except Exception as e:
             self.logger.error(f"OpenAI chats failed: {e}")
