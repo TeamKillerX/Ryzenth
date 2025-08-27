4b5148662abcc39a83003a789d0b5e621bca79bb
Randy Dev <killerx@randydev.my.id>
2025-08-27 10:38:35 +0700

    _

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -55,6 +55,7 @@ from .new_helper import (
     ChatsGeminiAsync,
     ChatsOpenAIAsync,
     ChatsQwenAsync,
+    ChatsGrokAsync,
     ImagesOpenAIAsync,
     ImagesOrgAsync,
     ImagesQwenAsync,
@@ -74,6 +75,7 @@ class RyzenthOrg:
         self._api_key = api_key
         self._session = None
         self._closed = False
+        self.grok_chat = ChatsGrokAsync(self)
         self.cohere_chat = ChatsCohereAsync(self)
         self.gemini_chat = ChatsGeminiAsync(self)
         self.old_gemini_chat = OldChatsGeminiAsync(self)
