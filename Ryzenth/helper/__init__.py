3e410c92b8dd7f89f042460f74a7ef8c739289ea
Randy Dev <killerx@randydev.my.id>
2025-08-28 23:14:37 +0700

    _

--- Ryzenth/helper/__init__.py
+++ Ryzenth/helper/__init__.py
@@ -31,13 +31,21 @@ from ._thinking import WhatAsync, WhatSync
 
 class Helpers:
     @classmethod
-    def encode_image_base64(image_path):
+    def encode_image_base64(cls, image_path):
         try:
             with open(image_path, "rb") as image_file:
                 return base64.b64encode(image_file.read()).decode('utf-8')
         except FileNotFoundError:
             return None
 
+class HelpersUseStatic:
+    @staticmethod
+    def encode_image_base64(image_path):
+        try:
+            with open(image_path, "rb") as image_file:
+                return base64.b64encode(image_file.read()).decode('utf-8')
+        except FileNotFoundError:
+            return None
 
 def to_buffer(
         response=None,
@@ -79,5 +87,6 @@ __all__ = [
     "AutoRetry",
     "to_buffer",
     "Helpers",
+    "HelpersUseStatic",
     "ResponseFileImage"
 ]
