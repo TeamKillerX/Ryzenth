04003a8034685725dd24e961e0291c62b495d047
xtsea <killerx@randydev.my.id>
2025-08-28 07:46:21 +0000

    pylint: auto fixes
    Signed-off-by: xtsea <killerx@randydev.my.id>

--- Ryzenth/new_helper/_deepseek_chats.py
+++ Ryzenth/new_helper/_deepseek_chats.py
@@ -30,6 +30,7 @@ from ..enums import ResponseType
 from ..helper import AutoRetry
 from ._models import RyzenthMessage
 
+
 class ChatsDeepseekAsync:
     def __init__(self, parent):
         self.parent = parent
