39cef03153d60b755250ac0e7619722773f83c6c
Randy Dev <killerx@randydev.my.id>
2025-08-28 11:34:11 +0700

    _

--- Ryzenth/new_helper/__init__.py
+++ Ryzenth/new_helper/__init__.py
@@ -13,6 +13,15 @@ from ._qwen_images import ImagesQwenAsync
 from ._qwen_videos import VideosQwenAsync
 from ._zai_chats import ChatsZaiAsync
 
+class RyzenthMessage:
+    @staticmethod
+    def core(content: str):
+        return {"role": "system", "content": content}
+
+    @staticmethod
+    def user(content: str):
+        return {"role": "user", "content": content}
+
 __all__ = [
     "ChatOrgAsync",
     "ChatsQwenAsync",
@@ -26,7 +35,8 @@ __all__ = [
     "ImagesQwenAsync",
     "ImagesOrgAsync",
     "ImagesOpenAIAsync",
-    "VideosQwenAsync"
+    "VideosQwenAsync",
+    "RyzenthMessage",
 ]
 
 __author__ = "Randy W @xtdevs, @xtsea"
