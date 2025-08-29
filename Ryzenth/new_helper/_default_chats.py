840845987515536bad4113f4b29627fbd2b89a29
Randy Dev <killerx@randydev.my.id>
2025-08-30 00:24:53 +0700

    _

--- Ryzenth/new_helper/_default_chats.py
+++ Ryzenth/new_helper/_default_chats.py
@@ -33,6 +33,7 @@ from .._errors import (
     InternalServerUltimateError,
     InvalidMessageError,
     WhatFuckError,
+    InvalidTypeError,
 )
 from .._export_class import ResponseResult
 from ..enums import ResponseType
@@ -138,7 +139,7 @@ class ChatOrgAsync:
                     dict) and not item for item in prompt):
                 raise EmptyMessageError("Prompt cannot be empty")
         else:
-            raise InvalidMessageError("Prompt type is invalid")
+            raise InvalidTypeError("Prompt type is invalid")
 
         try:
             use_turbo_fast = kwargs.pop("use_turbo_fast", False)
