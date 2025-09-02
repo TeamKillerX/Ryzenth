1af0bd1e1830d7e4c96617802e9fa87ebb253d96
Randy SL <killerx@randydev.my.id>
2025-09-02 11:15:16 +0700

    Fix missing import

--- Ryzenth/types/__init__.py
+++ Ryzenth/types/__init__.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from typing import Any, Optional
+from typing import Any, Optional, Callable
 
 from pydantic import BaseModel, Field
 
