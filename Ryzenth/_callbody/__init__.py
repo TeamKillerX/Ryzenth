7b20f50cfedd33305ccc0ee52db0d59a1753c3a8
Randy SL <killerx@randydev.my.id>
2025-09-04 21:52:37 +0700

    _

--- Ryzenth/_callbody/__init__.py
+++ Ryzenth/_callbody/__init__.py
@@ -2,10 +2,12 @@ from ._call_image_vision import ImagesVision
 from ._call_image_gemini_edit import ImagesGeminiEdit
 from ._call_image_openai import ImagesOpenAI
 from ._call_image_ghibli import ImagesGhibliFromOpenAI
+from ._call_image_turntext_openai import ImagesTurnTextOpenAI
 
 __all__ = [
     "ImagesVision",
     "ImagesGeminiEdit",
     "ImagesOpenAI",
-    "ImagesGhibliFromOpenAI"
+    "ImagesGhibliFromOpenAI",
+    "ImagesTurnTextOpenAI"
 ]
