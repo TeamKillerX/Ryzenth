a451f363013cbe462844491d8b853a7d6c6cb9f5
Randy SL <killerx@randydev.my.id>
2025-09-06 01:35:10 +0700

    _

--- Ryzenth/_callbody/_call_image_gemini_edit.py
+++ Ryzenth/_callbody/_call_image_gemini_edit.py
@@ -73,3 +73,5 @@ class ImagesGeminiEdit:
         except Exception as e:
             self.parent.logger.error(f"Gemini Image generation failed: {e}")
             raise InternalServerError(f"Gemini Image generation failed: {e}") from e
+        finally:
+            pass
