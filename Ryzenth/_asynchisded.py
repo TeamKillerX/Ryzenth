d4abd72b14e065027dbb20b1447f6650c75c4f2c
Randy SL <killerx@randydev.my.id>
2025-12-20 02:50:18 +0700

    Disable GhibliOrgAsync integration
    Comment out GhibliOrgAsync initialization and import.

--- Ryzenth/_asynchisded.py
+++ Ryzenth/_asynchisded.py
@@ -58,7 +58,7 @@ from .new_helper import (
     ChatsOpenAIAsync,
     ChatsQwenAsync,
     ChatsZaiAsync,
-    GhibliOrgAsync,
+    # GhibliOrgAsync,
     ImagesFluxAsync,
     ImagesOpenAIAsync,
     ImagesOrgAsync,
@@ -79,7 +79,7 @@ class RyzenthOrg:
         self._api_key = api_key
         self._session = None
         self._closed = False
-        self.ghibli = GhibliOrgAsync(self)
+        # self.ghibli = GhibliOrgAsync(self)
         self.hugging_chat = ChatsHuggingFaceAsync(self)
         self.flux_images = ImagesFluxAsync(self)
         self.zai_chat = ChatsZaiAsync(self)
